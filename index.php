62863b852fc3d08a667f9802f469e88302876b74
Mihir Patel <mihirp009@gmail.com>
2024-01-13 13:48:23 -0500

    asdsad

--- index.php
+++ index.php
@@ -1,5 +1,29 @@
+<html>
+
+<head>
+
+
+</head>
+
+<body>
+    <label>Youtube Link</label>
+    <input type="text" id="ytlink">
+    <label>Youtube Window Count</label>
+    <input type="text" id="ytnumber">
+    <button class="clickbtn">Click</button>
+</body>
 <script>
-    for (let i = 0; i <= 100; i++) {
-        window.open('https://www.youtube.com/shorts/rTUKreGbkac', '_blank', 'height=' + screen.height + ', width=' + screen.width);
-    }
+    const button = document.querySelector("button");
+    button.addEventListener("click", (event) => {
+        var ytlink = document.getElementById('ytlink')?.value;
+        var ytnumber = document.getElementById('ytnumber')?.value;
+        const button = document.querySelector("button");
+
+        for (let i = 0; i <= ytnumber; i++) {
+            window.open(ytlink, '_blank', 'height=' + screen.height + ', width=' + screen.width);
+        }
+
+    });
 </script>
+
+</html>
